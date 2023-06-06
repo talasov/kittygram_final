1e84d6fa4d802d33258afdc6bfe47b3b645500bb
talasov <talasov2017@yandex.ru>
2023-06-06 22:14:26 +0300

    update the project after review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG_SETTINGS')
 
