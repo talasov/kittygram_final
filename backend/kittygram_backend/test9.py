dc9a3de1061116e04248541a6477eca6055faf20
talasov <talasov2017@yandex.ru>
2023-06-07 00:20:15 +0300

    update the project after review

--- backend/kittygram_backend/test9.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Вывести значения переменных среды
-print(os.environ.get('POSTGRES_USER'))
-print(os.getenv('DB_NAME'))
--- backend/test12.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Загрузить переменные среды из файла .env
-load_dotenv()
-
-# Вывести значения переменных среды
-print(os.environ.get('POSTGRES_USER'))
-print(os.getenv('DB_NAME'))
